b326bfcc80f2d7653a5f045abceb8d042bcb5ef0
Boussouf Zohra <boussouf.zohra@gmail.com>
2020-09-11 12:29:56 +0200

    first commit

---
+++ header.php
@@ -0,0 +1,111 @@
+<!doctype html>
+<html lang="fr">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+    <title>Zohra Boussouf</title>
+    <!-- <link rel="icon" href="img/favicon.png" /> -->
+    <!-- Bootstrap CSS -->
+    <link crossorigin="anonymous" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" rel="stylesheet">
+    <link rel="stylesheet" href='public/css/app.css' />
+</head>
+
+<body>
+    <div class="fixed-top">
+        <!-- banniere -->
+        <div class="d-flex justify-content-center " id="banniere">
+            <img class=" w-75" src="/public/images/logozb.png" alt="Zohra Boussouf" id="logozb">
+        </div>
+
+        <!-- barre de navigation  -->
+        <nav class="navbar navbar-expand-lg navbar-light bg-light ">
+            <a class="navbar-brand" href="index.php"></a>
+            <button id="btnmenu" class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarContent">
+                <span class="navbar-toggler-icon"> <img style="width: 64px" src="public/images/favicon.png" alt="ZB"></span>
+            </button>
+
+            <div id="navbarContent" class="navbar-collapse collapse">
+                <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
+                    <li class="nav-item active">
+                        <a class="nav-link" href="#"> Présentation<span class="sr-only">(current)</span> </a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#"> Parcours Scolaire </a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#"> Parcours Professionel </a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#"> Activités </a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#"> Contact </a>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+    </div>
+
+
+    <!-- 
+<div class="container">
+
+        <div class="row">
+            <div class="col-12" id="header">
+            {% block titre %} {% endblock %}
+            </div>
+        </div>
+
+        <div class="row" id="carousel">
+            <div class="col">
+                {% block carousel1 %} {% endblock %}
+            </div>
+        </div>
+
+        <div class="row justify-content-lg-between " id="1">
+            <div class="col-12 col-lg-8">
+                {% block article1 %} {% endblock %}
+            </div>
+            <div class="col-12 col-lg-3 ">
+                {% block aside1 %} {% endblock %}
+            </div>
+         </div>
+
+        <div class="row justify-content-lg-between" id="2">
+            <div class="col-12 col-lg-8">
+                {% block article2 %} {% endblock %}
+            </div>
+            <div class="col-12 col-lg-3">
+                {% block aside2 %} {% endblock %}
+            </div>
+        </div>
+
+        <div class="row justify-content-lg-between" id="3">
+            <div class="col-12 col-lg-8">
+                {% block article3 %} {% endblock %}
+            </div>
+            <div class="col-12 col-lg-3">
+                {% block aside3 %}{% endblock %}
+            </div>
+        </div>
+
+        <div class="row justify-content-lg-between" id="4">
+            <div class="col-12 col-lg-8">
+                {% block article4 %} {% endblock %}
+            </div>
+            <div class="col-12 col-lg-3">
+                {% block aside4 %}{% endblock %}
+            </div>
+        </div>
+
+        <div class="row justify-content-lg-between" id="5">
+            <div class="col-12 col-lg-8">
+                {% block article5 %} {% endblock %}
+            </div>
+            <div class="col-12 col-lg-3">
+                {% block aside5 %}{% endblock %}
+            </div>
+        </div> -->
